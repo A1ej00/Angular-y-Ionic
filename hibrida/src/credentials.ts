06c453a6c369db348592402e7fa43833aef077ec
A1ej00 <146979603+A1ej00@users.noreply.github.com>
2024-12-14 18:58:37 -0500

    Guía 17

---
+++ hibrida/src/credentials.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyBdnYw-cNiwRl3e6DdhGxobrHOxEDgp-ik",
+    authDomain: "hibrida-866ad.firebaseapp.com",
+    projectId: "hibrida-866ad",
+    storageBucket: "hibrida-866ad.firebasestorage.app",
+    messagingSenderId: "147354432987",
+    appId: "1:147354432987:web:2d2231d35057a3aeb4e3fe"
+};
